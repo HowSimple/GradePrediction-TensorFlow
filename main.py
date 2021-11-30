6a11884e49e93026659c45cd81b606a564055e6e
Caleb Vaughn <43933950+HowSimple@users.noreply.github.com>
2021-11-30 15:51:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class GradePredictionNetwork:
 
         self.load_input(inputFile)
         self.prepare_datasets()
-        self.model()
 
         self.train( epochs)
         self.plot_error()
@@ -45,20 +44,20 @@ class GradePredictionNetwork:
         #self.train_labels = self.train_features['math score']
         self.train_features = self.train_features.drop(columns=label_columns)
 
-        print("Train features:",self.train_features.head())
-        print("Train labels:", self.train_labels.head())
+        #print("Train features:",self.train_features.head())
+        #print("Train labels:", self.train_labels.head())
         self.test_labels = self.test_features[label_columns]
         #self.test_labels = self.test_features["math score"]
 
         self.test_features = self.test_features.drop(columns=label_columns)
-        print("Test features:", self.test_features.head())
-        print("Test labels:", self.test_labels.head())
+        #print("Test features:", self.test_features.head())
+        #print("Test labels:", self.test_labels.head())
         self.valid_labels = self.valid_features[label_columns]
         #self.valid_labels = self.valid_features["math score"]
 
         self.valid_features = self.valid_features.drop(columns=label_columns)
-        print("Valid features:", self.valid_features.head())
-        print("Valid labels:", self.valid_labels.head())
+        #print("Valid features:", self.valid_features.head())
+        #print("Valid labels:", self.valid_labels.head())
 
 
 
@@ -67,38 +66,30 @@ class GradePredictionNetwork:
 
 
 
-    def model(self):
-        print(self.train_features.shape)
-
-        self.model = tf.keras.models.Sequential([
-    #tf.keras.layers.Dense(5, activation="relu", input_shape=tf.self.train_dat.shape[1:]),
-    tf.keras.layers.Dense(20, activation="relu"),
-    tf.keras.layers.Dense(15, activation="relu"),
-    tf.keras.layers.Dense(10, activation="relu"),
-
-    tf.keras.layers.Dense(self.train_labels.shape[1], activation="sigmoid")
-])
-
-
-
-    def predict(self):
-        grade_predictions = self.model.predict(self.test_labels)
-        df.to_csv(path_or_buf="PredictedStudentPerformance.csv" )
-        print(grade_predictions)
 
     def train(self, epochs):
+
+        self.model = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(20, activation="relu"),
+            #tf.keras.layers.Dense(15, activation="relu"),
+            # tf.keras.layers.Dense(10, activation="relu"),
+            # tf.keras.layers.Dense(5, activation="relu"),
+
+            tf.keras.layers.Dense(self.train_labels.shape[1], activation="relu")
+
+        ])
         self.model.compile(optimizer='rmsprop', loss='mse', metrics=['mae'])
 
         self.history = self.model.fit(self.train_features,
                                  self.train_labels,
+
                                  epochs=epochs,
                                       batch_size=64,
                                  # Suppress logging.
-                                 verbose=0,
+                                 verbose=1,
                                  validation_data= (self.test_features, self.test_labels)
                             )
 
-        print(self.model.trainable_variables)
 
     def plot_error(self):
         test_loss, test_acc = self.model.evaluate(self.test_features, self.test_labels, verbose=2)
@@ -107,7 +98,7 @@ class GradePredictionNetwork:
 
 
 
-        print('\nTest set accuracy:', test_acc)
+        print('\nTest set accuracy: ', test_acc)
 
 
         print('\nValid set accuracy:', valid_acc)
@@ -134,4 +125,4 @@ class GradePredictionNetwork:
 if __name__ == '__main__':
     print('Starting...')
 
-    GradePredictionNetwork("StudentsPerformance.csv", 1,1000)
+    GradePredictionNetwork("StudentsPerformance.csv", 1,50)
